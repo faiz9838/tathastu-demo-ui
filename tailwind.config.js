aeb8385355ca4ea4f54a9c7a804255b7cfacdd33
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-12-05 19:20:36 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
